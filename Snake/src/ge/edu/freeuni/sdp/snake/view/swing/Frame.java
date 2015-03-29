4793d13f59f9decf54274826590f2929174fe801
gigatsiklauri <gtsik11@freeuni.edu.ge>
2015-03-29 15:44:28 +0400

    Swing part of Views added

---
+++ Snake/src/ge/edu/freeuni/sdp/snake/view/swing/Frame.java
@@ -0,0 +1,27 @@
+package ge.edu.freeuni.sdp.snake.view.swing;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3975676773613683940L;
+
+	public void clearScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addLevel(int index, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
