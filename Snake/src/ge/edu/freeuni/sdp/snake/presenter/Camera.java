ef0016fee8d7ecca5147e247e18ca9b16573cdd6
Gio <ggogi12@freeuni.edu.ge>
2015-03-30 23:22:13 +0400

    implemented camera

---
+++ Snake/src/ge/edu/freeuni/sdp/snake/presenter/Camera.java
@@ -0,0 +1,48 @@
+package ge.edu.freeuni.sdp.snake.presenter;
+
+import ge.edu.freeuni.sdp.snake.model.Direction;
+import ge.edu.freeuni.sdp.snake.model.Point;
+import ge.edu.freeuni.sdp.snake.model.Size;
+
+public class Camera {
+	private Point center;
+
+	private Size camSize;
+	private Size boardSize;
+
+	/**
+	 * @param center
+	 * @param camSize
+	 * @param boardSize
+	 */
+	public Camera(Point center, Size camSize, Size boardSize) {
+		this.center = center;
+		this.camSize = camSize;
+		this.boardSize = boardSize;
+	}
+	
+	
+	public void step(Direction direction){
+		center = direction.addTo(center);
+	}
+	
+	
+	public boolean isVisible(Point p){
+		int width = camSize.getWidth();
+		int height = camSize.getHeight();
+		int boardWidth = boardSize.getWidth();
+		int boardHeight = boardSize.getHeight();
+		
+		
+		for(int i = center.X - width/2; i < center.X + width/2; i++){
+			for(int j = center.Y - height/2; j < center.Y + height/2; j++){
+				int x = i < boardWidth ? i : i-boardWidth;
+				int y = j < boardHeight ? j : j - boardHeight;
+				
+				if(x == p.X && y == p.Y) return true;
+			}
+		}
+		return false;
+
+	}
+}
