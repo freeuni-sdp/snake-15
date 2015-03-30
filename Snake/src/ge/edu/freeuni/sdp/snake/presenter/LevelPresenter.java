076b5f718f37638a545a5a40bbd39539f9612d80
gati3478 <giopetriashvili@gmail.com>
2015-03-30 23:52:13 +0400

    Added feature to notify observers on level selection
    To update description

--- Snake/src/ge/edu/freeuni/sdp/snake/presenter/LevelPresenter.java
+++ Snake/src/ge/edu/freeuni/sdp/snake/presenter/LevelPresenter.java
@@ -5,6 +5,7 @@ import ge.edu.freeuni.sdp.snake.model.Configuration;
 public class LevelPresenter {
 
 	private Configuration _config;
+	private LevelSelectionListener _listener;
 	
 	public LevelPresenter() {
 		this(Configuration.getInstance());
@@ -13,15 +14,26 @@ public class LevelPresenter {
 	public LevelPresenter(Configuration config) {
 		_config = config;
 	}
+	
+	public void setLevelSelectionListener(LevelSelectionListener listener) {
+		_listener = listener;
+	}
 
 	public String[] getLevelNames() {
 		return _config.getLevelNames();
-
 	}
 
 	public boolean setSelection(int index) {
 		if (index<0 || index>=getLevelNames().length) return false;
 		_config.selectLevel(index);
+		notifyLevelSelection();
 		return true;
 	}
+	
+	private void notifyLevelSelection() {
+		if (_listener == null)
+			return;
+		_listener.updateDescription(_config.getSelectedLevelDescription());
+	}
+	
 }
