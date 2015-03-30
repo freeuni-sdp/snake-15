3900e72816f9caa4ed1168f9836befa110aa9c03
Giorgi <giorgirokhadze@gmail.com>
2015-03-30 17:45:57 +0400

    Update SwingMazeView.java

--- Snake/src/ge/edu/freeuni/sdp/snake/view/swing/SwingMazeView.java
+++ Snake/src/ge/edu/freeuni/sdp/snake/view/swing/SwingMazeView.java
@@ -1,21 +1,77 @@
 package ge.edu.freeuni.sdp.snake.view.swing;
 
+import ge.edu.freeuni.sdp.snake.presenter.DirectionKey;
 import ge.edu.freeuni.sdp.snake.presenter.MazePresenter;
 import ge.edu.freeuni.sdp.snake.view.MazeView;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
-public class SwingMazeView implements MazeView {
-	private MazePresenter presenter;
+public class SwingMazeView implements MazeView, KeyListener {
+	@SuppressWarnings("unused")
 	private JFrame frame;
+	private DirectionKey key;
+	private MazePresenter presenter;
+
+	private void sleep() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public SwingMazeView(MazePresenter presenter, JFrame frame) {
-		this.presenter = presenter;
 		this.frame = frame;
+		this.presenter = presenter;
+		this.key = DirectionKey.None;
 	}
 
 	@Override
 	public void show() {
+		while (true) {
+			if (presenter.isGameOver())
+				return;
+			presenter.tick(key);
+			this.sleep();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			this.key = DirectionKey.Up;
+			break;
+
+		case KeyEvent.VK_DOWN:
+			this.key = DirectionKey.Down;
+			break;
+
+		case KeyEvent.VK_LEFT:
+			this.key = DirectionKey.Left;
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			this.key = DirectionKey.Right;
+			break;
+
+		default:
+			this.key = DirectionKey.None;
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
