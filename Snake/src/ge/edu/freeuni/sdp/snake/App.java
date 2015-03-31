4edf5f6a8462b7e19cb3501441d21742088bfb65
gigatsiklauri <gtsik11@freeuni.edu.ge>
2015-03-31 22:11:42 +0400

    changed design and swing is working

--- Snake/src/ge/edu/freeuni/sdp/snake/App.java
+++ Snake/src/ge/edu/freeuni/sdp/snake/App.java
@@ -6,22 +6,20 @@ import ge.edu.freeuni.sdp.snake.model.SingleMousePopulator;
 import ge.edu.freeuni.sdp.snake.model.Size;
 import ge.edu.freeuni.sdp.snake.model.SphericTopology;
 import ge.edu.freeuni.sdp.snake.view.ViewController;
-import ge.edu.freeuni.sdp.snake.view.terminal.TerminalViewFactory;
+import ge.edu.freeuni.sdp.snake.view.swing.SwingViewFactory;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.TerminalSize;
-
-public class App {
+public class App{
+	
+	private static final int GAME_WIDTH = 800;
+	private static final int GAME_HEIGHT = 400;
+	
 
 	public static void main(String[] args) {
 
-		Terminal terminal = getTerminal();
-		Size size = getSize(terminal);
+		Size size = new Size(GAME_WIDTH, GAME_HEIGHT);
 
 		List<Level> levels = new ArrayList<Level>();
 
@@ -35,28 +33,10 @@ public class App {
 
 		Configuration.init(size, levels);
 		
-		TerminalViewFactory viewFactory = new TerminalViewFactory(terminal);
+		
+		SwingViewFactory viewFactory = new SwingViewFactory();
 		ViewController controller = new ViewController(viewFactory);
 		controller.run();
 		
-		terminal.exitPrivateMode();
-	}
-
-	private static Terminal getTerminal() {
-		Terminal terminal = TerminalFacade.createTerminal(System.in,
-				System.out, Charset.forName("UTF8"));
-		terminal.enterPrivateMode();
-		terminal.clearScreen();
-		terminal.setCursorVisible(false);
-		return terminal;
-	}
-
-	private static Size getSize(Terminal terminal) {
-		TerminalSize terminalSize = terminal.getTerminalSize();
-		int width = terminalSize.getColumns();
-		int height = terminalSize.getRows();
-		
-		Size size = new Size(width, height);
-		return size;
 	}
 }
