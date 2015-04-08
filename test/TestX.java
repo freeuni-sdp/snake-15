002beba0329ece60563350215bc99f61864da962
gmamaladze <gmamaladze@gmail.com>
2015-04-08 14:43:26 +0200

    Added easy mock and test

--- test/TestX.java
+++ test/TestX.java
@@ -1,12 +1,28 @@
 
+import ge.edu.freeuni.sdp.snake.model.*;
+import static org.easymock.EasyMock.*;
+
+import org.easymock.MockType;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestX {
+
+	@Test
+	public void testClassic() {
+		Assert.assertTrue(true);
+	}
 	
 	@Test
-	public void TestAnyX() {
-		Assert.assertTrue(true);
+	public void testWithMock() {
+		PoisonBeing target = new PoisonBeing(null);
+		
+		Being mock = createMock(MockType.NICE, MouseBeing.class);
+		/* expect */ mock.kill();
+		replay(mock);
+		
+		target.interactWith(mock);
+		verify(mock);;
 	}
 
 }
