5f9e1441c2f99285f24b9c655d5f7a9545661f1a
Giorgi <giorgirokhadze@gmail.com>
2015-05-01 23:18:07 +0400

    Created junit test classes

---
+++ test/ge/edu/freeuni/sdp/snake/presenter/MazePresenterTest.java
@@ -0,0 +1,54 @@
+package ge.edu.freeuni.sdp.snake.presenter;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MazePresenterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMazePresenter() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCellUpdateListener() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsGameOver() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTick() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLivesUpdateListener() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveState() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestoreState() {
+		// fail("Not yet implemented");
+	}
+
+}
---
+++ test/ge/edu/freeuni/sdp/snake/view/terminal/TerminalGameOverViewTest.java
@@ -0,0 +1,34 @@
+package ge.edu.freeuni.sdp.snake.view.terminal;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TerminalGameOverViewTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTerminalGameOverView() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShow() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContinueGameOrNot() {
+		// fail("Not yet implemented");
+	}
+
+}
