f90a8b1e01d86fa4d7aba9314409dceaacf2503e
Giorgi <giorgirokhadze@gmail.com>
2015-05-03 18:26:46 +0400

    Creating tests for TerminalGameOverView

--- src/ge/edu/freeuni/sdp/snake/view/terminal/TerminalGameOverView.java
+++ src/ge/edu/freeuni/sdp/snake/view/terminal/TerminalGameOverView.java
@@ -12,6 +12,7 @@ public class TerminalGameOverView implements GameOverView {
 
 	private Terminal _terminal;
 	private boolean _continueGame;
+
 	public TerminalGameOverView(GameOverPresenter presenter, Terminal terminal) {
 		_terminal = terminal;
 		_continueGame = false;
@@ -20,36 +21,37 @@ public class TerminalGameOverView implements GameOverView {
 	@Override
 	public void show() {
 		_terminal.clearScreen();
-		String[] names = {
-			      "        Game Over        ",
-			      "Do you want to play again?",
-			      "  Press Y to play again." ,			      
-			      "  Press N or ESC to quit."
-		};
+		String[] names = { "        Game Over        ",
+				"Do you want to play again?", "  Press Y to play again.",
+				"  Press N or ESC to quit." };
 		TerminalSize screenSize = _terminal.getTerminalSize();
 
 		for (int i = 0; i < names.length; i++) {
-			_terminal.moveCursor(screenSize.getColumns()/2 - 15 ,screenSize.getRows()/2 - 2 + i);
+			_terminal.moveCursor(screenSize.getColumns() / 2 - 15,
+					screenSize.getRows() / 2 - 2 + i);
 			writeLine(String.format("%s", names[i]));
 		}
 
 		_terminal.flush();
 		while (true) {
 			Key p = _terminal.readInput();
-			if (p == null) continue;
+			if (p == null)
+				continue;
 			char ch = p.getCharacter();
-			if(p.getKind() == Kind.Escape || ch == 'n')
+			if (p.getKind() == Kind.Escape || ch == 'n')
 				break;
-			if(ch == 'y'){
+			if (ch == 'y') {
 				_continueGame = true;
 				break;
 			}
 		}
 	}
+
 	@Override
-	public boolean continueGameOrNot(){
+	public boolean continueGameOrNot() {
 		return _continueGame;
 	}
+
 	private void writeLine(String print) {
 		char[] printToChar = print.toCharArray();
 		for (int i = 0; i < print.length(); i++) {
