672f6909adb3d289e3848ec344733c2e763fa3a5
slezh12 <slezh12@freeuni.edu.ge>
2015-05-03 18:35:14 +0300

    back up step 2

--- test/ge/edu/freeuni/sdp/snake/presenter/LevelPresenterTest.java
+++ test/ge/edu/freeuni/sdp/snake/presenter/LevelPresenterTest.java
@@ -1,18 +1,15 @@
 package ge.edu.freeuni.sdp.snake.presenter;
 
-import ge.edu.freeuni.sdp.snake.model.Point;
 
 import org.junit.Before; 
 import org.junit.BeforeClass;
 import org.junit.Test; 
-import static org.mockito.Mockito.*;
 
 public class LevelPresenterTest {
 
 	
 	@BeforeClass
 	public void setUpClass(){
-		Point point = mock(Point.class);
 	}
 	
 	@Before
