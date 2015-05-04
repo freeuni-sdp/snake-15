65dd702b4032a915a9b7716158e1de121951b77d
Giorgi <giorgirokhadze@gmail.com>
2015-05-05 01:51:27 +0400

    Update MazePresenterTest.java
    Added some tests. Mainly added constructor tests and verifications of
    mocked object calls.

--- test/ge/edu/freeuni/sdp/snake/presenter/MazePresenterTest.java
+++ test/ge/edu/freeuni/sdp/snake/presenter/MazePresenterTest.java
@@ -1,31 +1,68 @@
 package ge.edu.freeuni.sdp.snake.presenter;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+
 import ge.edu.freeuni.sdp.snake.model.BeingKind;
+import ge.edu.freeuni.sdp.snake.model.Caretaker;
 import ge.edu.freeuni.sdp.snake.model.Direction;
 import ge.edu.freeuni.sdp.snake.model.GameFacade;
+import ge.edu.freeuni.sdp.snake.model.Memento;
 import ge.edu.freeuni.sdp.snake.model.Point;
 import ge.edu.freeuni.sdp.snake.model.Size;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class MazePresenterTest {
 
+	private static File file;
+	private static Size size;
+	private static Caretaker care;
 	private static GameFacade game;
+	private static Memento memento;
 	private static MazePresenter mazePresenter;
 
 	@Before
 	public void setUp() throws Exception {
+		file = Mockito.mock(File.class);
+		size = Mockito.mock(Size.class);
+		care = Mockito.mock(Caretaker.class);
 		game = Mockito.mock(GameFacade.class);
-		Mockito.when(game.getSize()).thenReturn(new Size(10, 10));
-		mazePresenter = Mockito.spy(new MazePresenter(game));
+		memento = Mockito.mock(Memento.class);
+
+		Mockito.when(size.getHeight()).thenReturn(10);
+		Mockito.when(size.getWidth()).thenReturn(10);
+		Mockito.when(file.exists()).thenReturn(false);
+		Mockito.when(game.getSize()).thenReturn(size);
+
+		mazePresenter = Mockito.spy(new MazePresenter(game, file, care));
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void testMazePresenterFileExists() {
+		Mockito.when(file.exists()).thenReturn(true);
+		Mockito.when(care.getMemento()).thenReturn(memento);
+		mazePresenter = new MazePresenter(game, file, care);
+		Mockito.verify(file, Mockito.atLeastOnce()).exists();
+		Mockito.verify(care).getMemento();
+	}
+
+	@Test
+	public void testMazePresenterFileNotExists() {
+		mazePresenter = new MazePresenter(game, file, care);
+		Mockito.verify(file, Mockito.atLeastOnce()).exists();
+		Mockito.verify(care, Mockito.never()).getMemento();
+	}
+
+	@Test
+	public void testMazePresenterOld() {
+		mazePresenter = new MazePresenter(game);
+		Mockito.verify(game, Mockito.atLeastOnce()).getSize();
 	}
 
 	@Test
